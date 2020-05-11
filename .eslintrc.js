7587fa955ff6ae66b7920d490ccd34192208455b
Gabriel Costa Siqueira <gsiqueira1996@gmail.com>
2020-05-10 23:39:23 -0300

    configurando typescript to development enviroment, eslint, nodemon, vscode

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {}
+}
